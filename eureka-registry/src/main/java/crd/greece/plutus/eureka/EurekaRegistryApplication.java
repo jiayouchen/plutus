b160c761bd7ce56a950b2d059495036a2cd3c8ce
jiayou.chen <cjy@sxw.cn>
2018-08-10 23:25:23 +0800

    初始化registry

--- eureka-registry/src/main/java/crd/greece/plutus/eureka/EurekaRegistryApplication.java
+++ eureka-registry/src/main/java/crd/greece/plutus/eureka/EurekaRegistryApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author chenjiayou022
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaRegistryApplication {
